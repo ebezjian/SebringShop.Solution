e8c1eaa57a0070bfec4dbdd7afbfb2e3f2f5b067
Ellie Lambert <ebezjian@gmail.com>
2021-01-22 12:23:21 -0500

    add shop models functionality

---
+++ SebringShop/Models/Shop.cs
@@ -0,0 +1,11 @@
+namespace SebringShop.Models
+{
+  public class Shop
+  {
+    public int ShopId { get; set; }
+    public string ShopName { get; set; }
+    public string ShopAddress { get; set; }
+    public string ShopType { get; set; }
+    
+  }
+}
