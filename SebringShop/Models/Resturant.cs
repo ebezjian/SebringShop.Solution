d110e07ba5a3c417fc5ec79aa50ab4b912399a80
Ellie Lambert <ebezjian@gmail.com>
2021-01-22 16:54:55 -0500

    add validation requirements for both resturant and shops.cs

--- SebringShop/Models/Resturant.cs
+++ SebringShop/Models/Resturant.cs
@@ -1,11 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace SebringShop.Models
 {
   public class Resturant
   {
     public int ResturantId { get; set; }
+    [Required]
+    [StringLength(30)]
     public string ResturantName { get; set; }
+    [Required]
     public string ResturantAddress { get; set; }
+    [Required]
     public string ResturantFoodType { get; set; }
+    [Required]
     
     public bool ResturantDelivery { get; set; }
   }
--- SebringShop/Models/Shop.cs
+++ SebringShop/Models/Shop.cs
@@ -1,10 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace SebringShop.Models
 {
   public class Shop
   {
     public int ShopId { get; set; }
+    [Required]
+    [StringLength(40)]
     public string ShopName { get; set; }
+    [Required]
     public string ShopAddress { get; set; }
+    [Required]
     public string ShopType { get; set; }
     
   }
