72d4da83a751dc1622bb9b4665e13289ac2edc8f
Ellie Lambert <ebezjian@gmail.com>
2021-01-22 18:16:10 -0500

    add resturant model in client side

---
+++ SebringClient.Solution/SebringClient/Models/Resturant.cs
@@ -0,0 +1,28 @@
+using System.Collections.Generic;
+using System;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace SebringClient.Models
+{
+  public class Resturant
+  {
+    public int ShopId { get; set; }
+
+    public string ResturantName { get; set; }
+
+    public string ResturantAddress { get; set; }
+
+    public string ResturantType { get; set; }
+    public bool ResturantDelivery { get; set; }
+    public static List<Resturant> GetResturants()
+    {
+      var apiCallTask = ApiHelper.GetAll();
+      var result = apiCallTask.Result;
+
+      JArray jsonResponse = JsonConvert.DeserializeObject<JArray>(result);
+      List<Resturant> shopList = JsonConvert.DeserializeObject<List<Resturant>>(jsonResponse.ToString());
+      return shopList;
+    }
+  }
+}
