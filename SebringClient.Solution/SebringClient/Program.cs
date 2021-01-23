95a0eb044cb2fca31c5760bcb597948543113dbb
Ellie Lambert <ebezjian@gmail.com>
2021-01-22 19:05:01 -0500

    changed caasing of sebringclient and sebring shops not sure if it will just totally break everything

--- SebringClient.Solution/SebringClient/Program.cs
+++ SebringClient.Solution/SebringClient/Program.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace sebringclient
+namespace SebringClient
 {
     public class Program
     {
--- SebringClient.Solution/SebringClient/Startup.cs
+++ SebringClient.Solution/SebringClient/Startup.cs
@@ -10,7 +10,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace sebringclient
+namespace SebringClient
 {
     public class Startup
     {
